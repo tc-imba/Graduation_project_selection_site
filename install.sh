161e1342b357f3a3c88e6312bc63a0cf1d9c9d0f
tc-imba <liuyh615@sjtu.edu.cn>
2017-09-16 14:01:21 +0800

    install script fix

--- install.sh
+++ install.sh
@@ -1,17 +1,6 @@
 #!/bin/bash
 
 apt-get install python3-pil
-pip3 install tornado openpyxl PyYaml
-
-if ! python3 -c "import mysql.connector" > /dev/null 2>&1; then
-    wget https://dev.mysql.com/get/Downloads/Connector-Python/mysql-connector-python-2.1.5.tar.gz
-    tar -zxf mysql-connector-python-2.1.5.tar.gz
-    (
-        cd mysql-connector-python-2.1.5
-        python3 setup.py install
-    )
-fi
-
-
+pip3 install tornado openpyxl PyYaml mysql-connector==2.1.6
 
 #mysql -u"$username" -p"$password" "$dbName" < ./sample.dump
