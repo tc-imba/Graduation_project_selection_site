a8d5e8389c7b39e939354449c2fc1d889cef8e40
tc-imba <liuyh615@sjtu.edu.cn>
2017-09-16 14:29:35 +0800

    fix login

--- controller/account.py
+++ controller/account.py
@@ -68,15 +68,13 @@ class UserRegisterHandler(BaseHandler):
             return
         u_name = self.get_secure_cookie('u_name_t').decode('UTF-8')
         uid = self.get_secure_cookie('username_t').decode('UTF-8')
-
-        udb = userDB(uid)
-        if len(udb.u_name) > 0:
-            self.redirect('/')
-            return
-
         role = self.get_secure_cookie('role').decode('UTF-8')
         gender = self.get_secure_cookie('gender').decode('UTF-8')
-        udb.newUser(u_name, role, phone, major, gender)
+        udb = userDB(uid)
+        if len(udb.u_name) > 0:
+            udb.updateUser(u_name, role, phone, major, gender)
+        else:
+            udb.newUser(u_name, role, phone, major, gender)
         self.set_secure_cookie("u_name", u_name)
         self.set_secure_cookie("username", str(uid))
         self.set_secure_cookie("vd", '')
